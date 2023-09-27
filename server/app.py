1db64408892995ee7056a22d8b15ae4ad3fcb8db
timothy <timothy.ndarau@student.moringaschool.com>
2023-09-27 12:36:50 +0300

    done

--- server/app.py
+++ server/app.py
@@ -3,7 +3,9 @@
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
+from bson import ObjectId
 from models import db, Plant
 
 app = Flask(__name__)
@@ -15,7 +17,6 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
-
 
 class Plants(Resource):
 
@@ -50,6 +51,45 @@ class PlantByID(Resource):
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
+app = Flask(__name__)
+
+# MongoDB connection setup
+client = MongoClient('mongodb://localhost:27017/')
+db = client['plants_db']
+collection = db['plants']
+
+# Update Route (PATCH)
+@app.route('/plants/<string:id>', methods=['PATCH'])
+def update_plant(id):
+    try:
+        updated_data = request.get_json()
+        plant = collection.find_one_and_update(
+            {'_id': ObjectId(id)},
+            {'$set': updated_data},
+            return_document=True
+        )
+
+        if not plant:
+            return jsonify({'error': 'Plant not found'}), 404
+
+        return jsonify(plant)
+    except Exception as e:
+        print(str(e))
+        return jsonify({'error': 'Internal server error'}), 500
+
+# Destroy Route (DELETE)
+@app.route('/plants/<string:id>', methods=['DELETE'])
+def delete_plant(id):
+    try:
+        result = collection.delete_one({'_id': ObjectId(id)})
+
+        if result.deleted_count == 0:
+            return '', 204  # No Content
+        else:
+            return jsonify({'error': 'Plant not found'}), 404
+    except Exception as e:
+        print(str(e))
+        return jsonify({'error': 'Internal server error'}), 500
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
